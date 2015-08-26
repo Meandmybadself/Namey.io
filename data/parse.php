476e2c8007a71fdf2c9101aa039f6b6eabd6249e
Jeffery Bennett <meandmybadself@gmail.com>
2015-08-25 23:39:41 -0500

    Initial commit.

---
+++ data/parse.php
@@ -0,0 +1,36 @@
+<?php
+    ini_set('auto_detect_line_endings',true);
+
+    $dir = "./names/";
+    $files = opendir($dir);
+
+    $allNames       = array();
+    $structNames    = array();
+
+    while(($f = readdir($files)) !== FALSE) {
+        $filename   = $dir . $f;
+        if ($f != '.DS_Store') {
+            $file       = fopen($filename, "r");
+            while(($data = fgetcsv($file)) !== FALSE) {
+                //Append name to $data.
+                $name   = $data[0];
+                $gender = $data[1];
+                $allNames[$name] = 1;
+                $structNames[$gender][$name] = 1;
+            }
+        }
+    }
+
+
+    //Make files.
+    //All names, shuffled.
+    $allNames = array_keys($allNames);
+    shuffle($allNames);
+    file_put_contents('all.json', json_encode($allNames));
+
+    //Struct names, shuffled.
+    foreach($structNames as $k=>$v) {
+        $arr = array_keys($structNames[$k]);
+        shuffle($arr);
+        file_put_contents($k . '.json', json_encode($arr));
+    }
